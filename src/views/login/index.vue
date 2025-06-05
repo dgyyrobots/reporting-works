e58fb518a057d576616869918d926b785a2d8f97
zhangjinjian <1016417093@qq.com>
2025-06-05 09:48:40 +0800

    style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -256,7 +256,7 @@ onMounted(() => {
     left: 0;
     right: 0;
     bottom: 0;
-    background: url('/@/assets/login_images/bg-pattern.png');
+    // background: url('/@/assets/login_images/bg-pattern.png');
     opacity: 0.05;
     pointer-events: none;
   }
