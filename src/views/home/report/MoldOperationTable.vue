f46e0f3eadbe4bbc7410b8620a17e131112f221a
zhangjinjian <1016417093@qq.com>
2025-06-20 13:17:35 +0800

    feat: 调模具table 字段

--- src/views/home/report/MoldOperationTable.vue
+++ src/views/home/report/MoldOperationTable.vue
@@ -3,56 +3,56 @@
     v-model:tableData="tableData" 
     :emptyRowTemplate="emptyRowTemplate"
   >
-    <el-table-column label="工作内容" prop="workContent" min-width="150" align="center">
+    <el-table-column label="工作内容" prop="work_content" min-width="150" align="center">
       <template #default="{ row }">
-        <el-input v-model="row.workContent" placeholder="" />
+        <el-input v-model="row.work_content" placeholder="" />
       </template>
     </el-table-column>
     
-    <el-table-column label="模具名称" prop="moldName" min-width="150" align="center">
+    <el-table-column label="模具名称" prop="mould_name" min-width="150" align="center">
       <template #default="{ row }">
         <div class="input-with-search">
-          <el-input v-model="row.moldName" placeholder="" />
+          <el-input v-model="row.mould_name" placeholder="" />
           <el-button class="search-btn"><Icon icon="svg-icon:search" /></el-button>
         </div>
       </template>
     </el-table-column>
     
-    <el-table-column label="模具编号" prop="moldCode" min-width="120" align="center">
+    <el-table-column label="模具编号" prop="mould_no" min-width="120" align="center">
       <template #default="{ row }">
-        <el-input v-model="row.moldCode" placeholder="" />
+        <el-input v-model="row.mould_no" placeholder="" />
       </template>
     </el-table-column>
     
-    <el-table-column label="模具型号" prop="moldModel" min-width="120" align="center">
+    <el-table-column label="模具型号" prop="mould_model" min-width="120" align="center">
       <template #default="{ row }">
-        <el-input v-model="row.moldModel" placeholder="" />
+        <el-input v-model="row.mould_model" placeholder="" />
       </template>
     </el-table-column>
     
-    <el-table-column label="模具操作类型" prop="operationType" min-width="120" align="center">
+    <el-table-column label="模具操作类型" prop="io_type_name" min-width="120" align="center">
       <template #default="{ row }">
-        <el-select v-model="row.operationType" placeholder="请选择">
-          <el-option label="领用" value="1" />
-          <el-option label="归还" value="2" />
-          <el-option label="维修" value="3" />
-          <el-option label="上模" value="4" />
-          <el-option label="下模" value="5" />
-          <el-option label="报废" value="6" />
+        <el-select v-model="row.io_type_name" placeholder="请选择">
+          <el-option label="领用" value="领用" />
+          <el-option label="归还" value="归还" />
+          <el-option label="维修" value="维修" />
+          <el-option label="上模" value="上模" />
+          <el-option label="下模" value="下模" />
+          <el-option label="报废" value="报废" />
         </el-select>
       </template>
     </el-table-column>
     
-    <el-table-column label="数量" prop="quantity" min-width="100" align="center">
+    <el-table-column label="数量" prop="uqty" min-width="100" align="center">
       <template #default="{ row }">
-        <el-input-number v-model="row.quantity" :min="0" :precision="0" controls-position="right" />
+        <el-input v-model="row.uqty"  />
       </template>
     </el-table-column>
     
-    <el-table-column label="备注" prop="remark" min-width="200" align="center">
+    <el-table-column label="备注" prop="note" min-width="200" align="center">
       <template #default="{ row }">
         <el-input 
-          v-model="row.remark" 
+          v-model="row.note" 
           type="textarea" 
           :rows="2"
           placeholder="" 
@@ -69,13 +69,13 @@ import BaseTable from './components/BaseTable.vue'
 
 // 空行模板
 const emptyRowTemplate = {
-  workContent: '',
-  moldName: '',
-  moldCode: '',
-  moldModel: '',
-  operationType: '',
-  quantity: 0,
-  remark: ''
+  work_content: '',
+  mould_name: '',
+  mould_no: '',
+  mould_model: '',
+  io_type_name: '',
+  uqty: null,
+  note: ''
 }
 
 // 表格数据
